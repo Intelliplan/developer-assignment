27bb182b428640cd806edd7c082ec1646be2dbce
Kristoffer Roupe <kristoffer.roupe@intelliplan.se>
2014-12-16 15:35:07 +0100

    Added gemfile

---
+++ lib/candidate_locator.rb
@@ -0,0 +1,2 @@
+class CandidateLocator
+end
---
+++ spec/candidate_locator_spec.rb
@@ -0,0 +1,7 @@
+require 'candidate_locator'
+
+describe CandidateLocator, "#locate" do
+  it "should be done" do
+    expect(true).to be false
+  end
+end
